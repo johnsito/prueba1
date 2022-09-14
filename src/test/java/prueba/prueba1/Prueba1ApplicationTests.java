60a89e5b2a83ef06151c011acaf06ea67b5c337a
johnsito <jotazo@hotmail.com>
2022-09-13 21:22:45 -0500

    Merge with Development

---
+++ src/test/java/prueba/prueba1/Prueba1ApplicationTests.java
@@ -0,0 +1,14 @@
+package prueba.prueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootTest
+class Prueba1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
