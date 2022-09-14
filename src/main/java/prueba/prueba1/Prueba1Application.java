a90e5cc33fc17cfddb8d64de9d3ecb4196cabd7d
johnsito <jotazo@hotmail.com>
2022-09-13 21:52:36 -0500

    Arreglando la pagina principal "Home"

--- src/main/java/prueba/prueba1/Prueba1Application.java
+++ src/main/java/prueba/prueba1/Prueba1Application.java
@@ -7,18 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class Prueba1Application {
 
-    @GetMapping("/")
-    public String home(){
-
-        return "Bienvenidos a la app de Creative Minds" +
-                "<br> <img src=\"https://miviaje.com/wp-content/uploads/2018/09/cano-cristales.jpg\" alt=\"modelado\">";
-    }
-
-    @GetMapping({"/error"})
-    public String getError(){
-        return "error";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Prueba1Application.class, args);
     }
--- src/main/java/prueba/prueba1/controller/appController.java
+++ src/main/java/prueba/prueba1/controller/appController.java
@@ -1,7 +1,12 @@
+package prueba.prueba1.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class appController {
-
+    @GetMapping({"/"})
+    public String home(){
+        return "home";
+    }
 }
